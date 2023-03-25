b0b2c4171ae080cfbf28d421e75dfe48519f54e9
Josh <108093283+bigbootylatinas@users.noreply.github.com>
2023-03-25 15:55:55 -0400

    Update Last MewHub update.lua

--- Last MewHub update.lua
+++ Last MewHub update.lua
@@ -2,7 +2,7 @@
 
 
 --getgenv().WishList = {"Pidgey"} 
---getgenv().Webhook = ""
+--getgenv().Webhook = "your webhook here"
 
 
 --[[--]] -- Global Variables
@@ -435,7 +435,7 @@ local ReportData = {
                 ['url'] = webhookdogg
             }, 
             ["image"] = {
-            ["url"] = "https://cdn.discordapp.com/attachments/1089249367829004388/1089257203896172675/9787.gif",
+            ["url"] = "https://media.discordapp.net/attachments/1045266138386403388/1084837866389131355/cozy.gif",
             --["url"] = headshot,
             
         },
